adb152bcab67843b2f8771d02666c976224f26d9
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-04-25 12:59:53 +0530

    [Shruthin]Added routes for books api

--- src/routes/book.route.js
+++ src/routes/book.route.js
@@ -1,15 +1,15 @@
-// import express from 'express';
-// import * as bookController from '../controllers/book.controller';
-// import { newBookValidator } from '../validators/user.validator';
-// import { userAuth } from '../middlewares/auth.middleware';
-// const router = express.Router();
+import express from 'express';
+import * as bookController from '../controllers/book.controller';
+import { newBookValidator } from '../validators/user.validator';
+import { userAuth } from '../middlewares/auth.middleware';
+const router = express.Router();
 
-// router.post('', newBookValidator, bookController.postBook);
+router.post('', newBookValidator, bookController.postBook);
 
-// router.get('',userAuth, bookController.getAllBooks);
-
-
+router.get('',userAuth, bookController.getAllBooks);
 
 
 
-// export default router;
+
+
+export default router;
