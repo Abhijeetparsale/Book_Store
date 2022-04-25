750542a134b349a33198788880068f794dd63f28
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-04-25 13:00:23 +0530

    [Shruthin]Added service for books api

--- src/services/book.service.js
+++ src/services/book.service.js
@@ -1,27 +1,27 @@
-// import Book from '../models/book.model';
+import Book from '../models/book.model';
 
 
 
 
-// export const postBook = async (body) => {
-//     const bookDetails = await Book.findOne({ Title: body.Title})
-//     // console.log(bookDetails)
-//     if (bookDetails) {
-//         throw new Error('Book already posted')
-//     }
-//     else {
-//         const data = await Book.create(body);
-//         return data;
-//     }
-// };
+export const postBook = async (body) => {
+    const bookDetails = await Book.findOne({ Title: body.Title})
+    // console.log(bookDetails)
+    if (bookDetails) {
+        throw new Error('Book already posted')
+    }
+    else {
+        const data = await Book.create(body);
+        return data;
+    }
+};
 
-// export const getAllBooks = async () => {
-//     const data = await Book.find();
-//    console.log(data)
-//     if (data.length === 0) {
-//         throw new Error("No Book Present")
-//     }
-//     else {
-//         return data;
-//     }
-// };
+export const getAllBooks = async () => {
+    const data = await Book.find();
+   console.log(data)
+    if (data.length === 0) {
+        throw new Error("No Book Present")
+    }
+    else {
+        return data;
+    }
+};
