cca1a27be8d8c2f9aa64b3766858373884ba87a9
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-05-01 11:25:38 +0530

    initial commits

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,19 +3,22 @@ const router = express.Router();
 
 import userRoute from './user.route';
 import bookRoute from './book.route';
-
+import cartRoute from './cart.route';
+import wishlistRoute from './wishlist.route';
 /**
  * Function contains Application routes
  *
  * @returns router
  */
-const routes = () => {
+function routes() {
   router.get('/', (req, res) => {
     res.json('Welcome To BookStore');
   });
   router.use('/users', userRoute);
   router.use('/book', bookRoute);
+  router.use('/cart', cartRoute);
+  router.use('/list', wishlistRoute)
   return router;
-};
+}
 
 export default routes;
---
+++ src/routes/wishlist.route.js
@@ -0,0 +1,16 @@
+// import express from 'express';
+// import * as wishlistController from '../controllers/wishlist.controller';
+// import { userAuth } from '../middlewares/auth.middleware';
+
+// const router = express.Router();
+
+//  //to create a wishlist
+//  router.post('/wishlist/:_id', userAuth, wishlistController.addWishlist);
+
+//  //to get all items of wishlist
+//  router.get('/getlist', userAuth,  wishlistController.getWishlist);
+
+//  //to delete wishlist items
+//  router.put('/dellist/:id', userAuth, wishlistController.delWishlist)
+
+//  export default router;
