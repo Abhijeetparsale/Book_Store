49f788b82f2de903ac1c3e16f02b1b29ef3504a8
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-05-01 11:32:22 +0530

    [Shruthin] Added 3 routes for wishlist

--- src/routes/wishlist.route.js
+++ src/routes/wishlist.route.js
@@ -1,16 +1,16 @@
-// import express from 'express';
-// import * as wishlistController from '../controllers/wishlist.controller';
-// import { userAuth } from '../middlewares/auth.middleware';
+import express from 'express';
+import * as wishlistController from '../controllers/wishlist.controller';
+import { userAuth } from '../middlewares/auth.middleware';
 
-// const router = express.Router();
+const router = express.Router();
 
-//  //to create a wishlist
-//  router.post('/wishlist/:_id', userAuth, wishlistController.addWishlist);
+ //to create a wishlist
+ router.post('/wishlist/:_id', userAuth, wishlistController.addWishlist);
 
-//  //to get all items of wishlist
-//  router.get('/getlist', userAuth,  wishlistController.getWishlist);
+ //to get all items of wishlist
+ router.get('/getlist', userAuth,  wishlistController.getWishlist);
 
-//  //to delete wishlist items
-//  router.put('/dellist/:id', userAuth, wishlistController.delWishlist)
+ //to delete wishlist items
+ router.put('/dellist/:id', userAuth, wishlistController.delWishlist)
 
-//  export default router;
+ export default router;
