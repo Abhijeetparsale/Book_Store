2fe276420c4b1d987c42981e92f94479b232a438
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-05-04 22:08:06 +0530

    [Shruthin] Refactored user input fields

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,11 +3,10 @@ import { Schema, model } from 'mongoose';
 const userSchema = new Schema(
   {
    
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, required: true}
+    fullName: { type: String,  },
+    email: { type: String, unique: true },
+    password: { type: String },
+    mobileNum: { type: String}
   },
   {
     timestamps: true
