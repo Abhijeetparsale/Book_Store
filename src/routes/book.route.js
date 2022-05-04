4ab83e672b942119c999e8b17a1c16ee867f8d0d
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-05-04 22:08:42 +0530

    [Shruthin] Added redis middleware in book route

--- src/routes/book.route.js
+++ src/routes/book.route.js
@@ -2,11 +2,13 @@ import express from 'express';
 import * as bookController from '../controllers/book.controller';
 import { newBookValidator } from '../validators/user.validator';
 import { userAuth } from '../middlewares/auth.middleware';
+import { checkgetAllBooks  } from '../middlewares/redis.middleware';
+
 const router = express.Router();
 
 router.post('', newBookValidator, bookController.postBook);
 
-router.get('',userAuth, bookController.getAllBooks);
+router.get('',checkgetAllBooks, bookController.getAllBooks);
 
 
 
